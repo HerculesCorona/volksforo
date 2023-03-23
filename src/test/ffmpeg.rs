7f39c041f92be1204d06e10286850d00087054d1
Joshua Moon <josh@jaw.sh>
2023-03-23 19:40:05 +0100

    quick blake3 test

--- src/test/ffmpeg.rs
+++ src/test/ffmpeg.rs
@@ -1,5 +1,26 @@
+use std::fs::File;
+use std::io::{BufReader, Read};
 use std::path::Path;
+
 extern crate ffmpeg_the_third as ffmpeg;
+
+#[test]
+fn test_blake3() {
+    let f = File::open("src/test/static/text.jpg").unwrap();
+    let mut reader = BufReader::new(f);
+    let mut buffer = Vec::new();
+
+    reader.read_to_end(&mut buffer).unwrap();
+
+    let mut hasher = blake3::Hasher::new();
+    hasher.update(buffer.as_slice());
+
+    let hash = hasher.finalize();
+    assert_eq!(
+        hash.to_string(),
+        "8770325564caf30faf3fcfc0ff5be1a6e06afb405b8d1ce647b7dd4a694f6754".to_string()
+    );
+}
 
 #[test]
 fn test_ffmpeg() {
@@ -19,7 +40,38 @@ fn test_ffmpeg() {
             .name()
             .to_owned();
 
-        println!("Testing file: {:?}", file);
         assert_eq!(format_name, file_format_name);
+    }
+}
+
+#[test]
+fn test_infer() {
+    let vec = vec![
+        (
+            infer::get_from_path("src/test/static/text.gif"),
+            "gif",
+            "image/gif",
+        ),
+        (
+            infer::get_from_path("src/test/static/text.jpg"),
+            "jpg",
+            "image/jpeg",
+        ),
+        (
+            infer::get_from_path("src/test/static/text.png"),
+            "png",
+            "image/png",
+        ),
+        (
+            infer::get_from_path("src/test/static/text.webp"),
+            "webp",
+            "image/webp",
+        ),
+    ];
+
+    for (file, ext, mime) in vec {
+        let res = file.unwrap().unwrap();
+        assert_eq!(res.mime_type(), mime);
+        assert_eq!(res.extension(), ext);
     }
 }
